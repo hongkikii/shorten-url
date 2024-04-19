4e2201ee15f74958eae3cc0338ef51d499c96233
hongkikii <hyangmeeh@icloud.com>
2024-04-19 13:31:45 +0900

    feat: 단축키 정보 조회 API controller, dto 구현

--- src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlRestController.java
+++ src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlRestController.java
@@ -3,6 +3,7 @@ package kr.co.shortenurlservice.presentation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,9 @@ public class ShortenUrlRestController {
 
     // 정보 조회
     @GetMapping("/shortenUrl/{shortenUrlkey}")
-    public ResponseEntity<?> getShortenUrlInformation() {
+    public ResponseEntity<ShortenUrlInformationDto> getShortenUrlInformation(
+            @PathVariable String shortenUrlkey
+    ) {
         return ResponseEntity.ok().body(null);
     }
 }
